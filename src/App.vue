32e65e63ba285a6d0b72451814eaf1f531b3821d
Baran <me@baran.wang>
2019-10-23 15:38:27 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,118 @@
+<style scoped>
+table {
+  text-align: center;
+}
+table td,
+table th {
+  padding: .25em 1em;
+}
+</style>
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <fieldset>
+      <legend>Persons</legend>
+      <div
+        v-for="(item, index) in persons"
+        :key="index"
+      >
+        <input
+          v-model="item.name"
+          type="text"
+          placeholder="name"
+        >
+        <input
+          v-model="item.price "
+          type="number"
+          placeholder="price"
+        >
+        <button @click="persons.splice(index, 1)">
+          -
+        </button>
+      </div>
+      <button @click="persons.push({ name: '', price: null })">
+        + New Person
+      </button>
+    </fieldset>
+    <fieldset>
+      <legend>Public</legend>
+      <input
+        v-model="expressFee"
+        type="text"
+        placeholder="Express Fee"
+      >
+      <input
+        v-model="packageFee"
+        type="text"
+        placeholder="Package Fee"
+      >
+      <hr>
+      <input
+        v-model="totalFee"
+        type="text"
+        placeholder="Total Fee"
+      >
+    </fieldset>
+    <!-- <button @click="compute">
+      Compute
+    </button>-->
+    <table>
+      <thead>
+        <tr>
+          <th>Person</th>
+          <th>Price</th>
+          <th>Public Fee</th>
+          <th>Total Fee</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, index) in result"
+          :key="index"
+        >
+          <td v-text="item.name" />
+          <td v-text="item.price" />
+          <td v-text="item.publicFee" />
+          <td>{{ item.price + item.publicFee }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      persons: [
+        {
+          name: "",
+          price: null
+        },
+        {
+          name: "",
+          price: null
+        }
+      ],
+      expressFee: null,
+      packageFee: null,
+      totalFee: null
+    };
+  },
+  computed: {
+    result() {
+      const publicFee = Number(this.expressFee) + Number(this.packageFee);
+      const original = this.persons.reduce(
+        (total, item) => (total += Number(item.price)),
+        0
+      );
+      const discount = (this.totalFee - publicFee) / original;
+      return this.persons.map(item => ({
+        name: item.name,
+        price: +(item.price * discount).toFixed(2),
+        publicFee: +(publicFee / this.persons.length).toFixed(2)
+      }));
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
